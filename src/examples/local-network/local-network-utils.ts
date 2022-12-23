8d57ec39111c0dc890a9875e3bd4f3c50d54254e
Ruben Gutierrez <rubdeivis@gmail.com>
2022-12-23 09:24:51 -0400

    fix config

--- src/examples/local-network/local-network-utils.ts
+++ src/examples/local-network/local-network-utils.ts
@@ -1,11 +1,6 @@
 export const localNetworkUtils = {
-  relayRpc: "ws://127.0.0.1:41089",
-  parachain1Rpc: "ws://127.0.0.1:34615",
+  relayRpc: "ws://127.0.0.1:30101",
+  parachain1Rpc: "ws://127.0.0.1:40001",
   parachain1ChainId: 1000,
-  parachain1DestinationAccount:
-    "HnMAUz7r2G8G3hB27SYNyit5aJmh2a5P4eMdDtACtMFDbam",
-  parachain2Rpc: "ws://127.0.0.1:39413",
-  parachain2ChainId: 2000,
-  parachain2DestinationAccount:
-    "5HGjWAeFDfFCWPsjFQdVV2Msvz2XtMktvgocEZcCj68kUMaw",
+  parachain1DestinationAccount: "",
 };
--- src/examples/rococo/rococo-examples-utils.ts
+++ src/examples/rococo/rococo-examples-utils.ts
@@ -3,11 +3,9 @@ export const rococoExampleUtils = {
   rockMineRpc: "wss://rococo-rockmine-rpc.polkadot.io",
   rockMineParachainId: 1000,
   daliParachainId: 2087,
-  senderMnemonic:
-    "trick grow portion canal wide loan clutch improve appear very security melt",
-  rockmineDestinationAccount:
-    "5H8khHPPPUY9byAcR12KgbzoJuRwe5LR2nNeRBKZADz4b38v",
+  senderMnemonic: "",
+  rockmineDestinationAccount: "",
   daliDestinationAccount: "",
-  rococoDestinationAccount: "5GBSRQAxsaaVik8B2tdZPHwFo98peMEmiEqk6HR95SyESv3V",
+  rococoDestinationAccount: "",
   rocAmount: 500000000000, // 0.05 ROC
 };
