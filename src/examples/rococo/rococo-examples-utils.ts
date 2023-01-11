516649fda930f0fe5936bc56477c74b90d7221ae
Ruben Gutierrez <45153592+RubenGutierrezC@users.noreply.github.com>
2023-01-11 12:07:02 -0400

    No limited methods (#7)
    * evaluation fixes
    
    * update doc
    
    * no limited methods + update doc

--- src/examples/rococo/rococo-examples-utils.ts
+++ src/examples/rococo/rococo-examples-utils.ts
@@ -2,10 +2,12 @@ export const rococoExampleUtils = {
   rococoRpc: 'wss://rococo-rpc.polkadot.io',
   rockMineRpc: 'wss://rococo-rockmine-rpc.polkadot.io',
   rockMineParachainId: 1000,
+  mangataParachainId: 2110,
   daliParachainId: 2087,
   senderMnemonic: '',
   rockmineDestinationAccount: '',
   daliDestinationAccount: '',
   rococoDestinationAccount: '',
-  rocAmount: 500000000000, // 0.05 ROC
+  mangataDestinationAccount: '',
+  rocAmount: 500000000000, // 0.5 ROC
 }
